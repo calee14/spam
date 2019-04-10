92357b071a5ef12e570664ed879440f3af067127
calee14 <caplee1000@gmail.com>
2019-04-10 12:44:04 -0700

    iterating through the email content

--- yougotspam/content.js
+++ yougotspam/content.js
@@ -1,18 +1,22 @@
 // Listen for messages
-// chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-//     // If the received message has the expected format...
-//     if (msg.text === 'report_back') {
-//         // Call the specified callback, passing
-//         // the web-page's DOM content as argument
-//         var domContent = document.getElementById(":mp");
-//         // console.log(document.all[0].textContent);
-//         console.log('I received the following DOM content:\n');
-// 	    console.log(domContent)
-// 	    console.log(domContent.innerHTML)
-//         sendResponse(document.all[0].outerHTML);
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    // If the received message has the expected format...
+    if (msg.text === 'report_back') {
+        // Call the specified callback, passing
+        // the web-page's DOM content as argument
+        var list= document.getElementsByClassName("ii");
+		for (var i = 0; i < list.length; i++) {
+		    console.log(list[i].innerText);
+		}
+        var domContent = document.getElementById(":mp");
+        // console.log(document.all[0].textContent);
+        console.log('I received the following DOM content:\n');
+	    console.log(domContent)
+	    console.log(domContent.innerHTML)
+        sendResponse(document.all[0].outerHTML);
         
-//     }
-// });
+    }
+});
 
 // Listen for messages
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
