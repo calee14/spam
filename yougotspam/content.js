a6f7a1579d8a21f1846bfc46cc1c6f3c539ed4a7
calee14 <caplee1000@gmail.com>
2019-04-10 11:51:20 -0700

    content js now retrieves the selected text then sends it to backend

--- yougotspam/content.js
+++ yougotspam/content.js
@@ -1,10 +1,27 @@
 // Listen for messages
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    // If the received message has the expected format...
-    if (msg.text === 'report_back') {
-        // Call the specified callback, passing
-        // the web-page's DOM content as argument
-        sendResponse(document.all[0].outerHTML);
-        console.log(document.all[0].textContent);
-    }
+// chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+//     // If the received message has the expected format...
+//     if (msg.text === 'report_back') {
+//         // Call the specified callback, passing
+//         // the web-page's DOM content as argument
+//         var domContent = document.getElementById(":mp");
+//         // console.log(document.all[0].textContent);
+//         console.log('I received the following DOM content:\n');
+// 	    console.log(domContent)
+// 	    console.log(domContent.innerHTML)
+//         sendResponse(document.all[0].outerHTML);
+        
+//     }
+// });
+
+// Listen for messages
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+	// If the recieived request has the expected format...
+    if (request.method == "getSelection")
+		// Call the specified callback, passing
+        // the selected text
+		sendResponse({data: window.getSelection().toString()});
+    else
+    	// Call the specified callback passing no infomration
+    	sendResponse({}); // snub them.
 });
