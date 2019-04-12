5956355de54821a8ec1b0d1dc88132d606513347
calee14 <caplee1000@gmail.com>
2019-04-11 17:11:34 -0700

    adding evaluating code to check model

---
+++ api/evaluate.py
@@ -0,0 +1,20 @@
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import EarlyStopping
+
+test_sequences = tok.texts_to_sequences(X_test)
+test_sequences_matrix = sequence.pad_sequences(test_sequences,maxlen=max_len)
+
+accr = model.evaluate(test_sequences_matrix,Y_test)
+
+print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
+
+texts = ["Hi what's up"]
+seq = tok.texts_to_sequences(texts)
+seq = sequence.pad_sequences(seq,maxlen=max_len)
+result = model.predict(seq)
+print(result)
