b749c4cec2b6efe304f70938a9093546bde4161f
calee14 <caplee1000@gmail.com>
2019-04-11 17:10:03 -0700

    trainging the model of the preprocessed data

---
+++ api/train.py
@@ -0,0 +1,34 @@
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import EarlyStopping
+
+max_words = 1000
+max_len = 150
+tok = Tokenizer(num_words=max_words)
+tok.fit_on_texts(X_train)
+sequences = tok.texts_to_sequences(X_train)
+print(sequences[0])
+sequences_matrix = sequence.pad_sequences(sequences,maxlen=max_len)
+
+def RNN():
+    inputs = Input(name='inputs',shape=[max_len])
+    layer = Embedding(max_words,50,input_length=max_len)(inputs)
+    layer = LSTM(64)(layer)
+    layer = Dense(256,name='FC1')(layer)
+    layer = Activation('relu')(layer)
+    layer = Dropout(0.5)(layer)
+    layer = Dense(1,name='out_layer')(layer)
+    layer = Activation('sigmoid')(layer)
+    model = Model(inputs=inputs,outputs=layer)
+    return model
+
+model = RNN()
+model.summary()
+model.compile(loss='binary_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
+
+model.fit(sequences_matrix,Y_train,batch_size=128,epochs=10,
+          validation_split=0.2,callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)])
