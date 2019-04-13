8520c611627495316f3b7376030015fe39b0ce3b
calee14 <caplee1000@gmail.com>
2019-04-12 21:43:14 -0700

    jsonify repsonse for all routes

--- api/main.py
+++ api/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/'):
 def index():
 	return jsonify("Hello World")
-	
+
 @app.route('/api/v1.0/<message>')
 def api(message):
-	return message
+	return jsonify(message)
