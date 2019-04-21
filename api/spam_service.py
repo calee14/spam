154b6716c33f649e3399eb03a21e7d8470c97596
calee14 <caplee1000@gmail.com>
2019-04-21 16:15:06 -0700

    loading model for heroku server

--- api/spam_service.py
+++ api/spam_service.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding
 from tensorflow.keras.optimizers import RMSprop
@@ -10,11 +11,16 @@ from tensorflow.keras.models import load_model
 
 class spam_service(object):
     def __init__(self):
+        print('This is where we are')
+        print(os.path.dirname(os.path.realpath(__file__)))
+        for path, subdirs, files in os.walk(os.path.dirname(os.path.realpath(__file__))):
+            for name in files:
+                print(os.path.join(path, name))
         self.max_len = 150
         # load the model
-        self.model = load_model('spam_detector.hdf5')
+        self.model = load_model(os.path.dirname(os.path.realpath(__file__)) + '/spam_detector.hdf5')
         # loading the tokenizer
-        with open('tok.pkl', 'rb') as handle:
+        with open(os.path.dirname(os.path.realpath(__file__)) + '/tok.pkl', 'rb') as handle:
             self.tok = pickle.load(handle)
     def predict(self, text):
         '''
@@ -25,6 +31,6 @@ class spam_service(object):
         seq = sequence.pad_sequences(seq,maxlen=self.max_len)
         result = self.model.predict(seq)
         return result
-
-s = spam_service()
-print(s.predict(['Hello World']))
+#
+# s = spam_service()
+# print(s.predict(['Hello World']))
